f81d16fc98fbd973746548eda4a56fbc4c41e5c1
Jun <youngjun.woo.data@gmail.com>
2023-05-12 16:22:23 -0400

    eda demographic variables

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np 
 import seaborn as sns 
 import matplotlib.pyplot as plt 
+import matplotlib.patches as mpatches
+
 
 # %%
 def my_histogram(df, column_name):
